4c88953f7b4fbdc7855d9b5adb0850ef1f773e67
Sagar Patade <sagarpatade333@gmail.com>
2026-02-23 16:28:58 +0530

    fix: resolve output naming and self-reference errors

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,12 +1,10 @@
-# modules/alb/outputs.tf
-
 output "target_group_arn" {
   description = "The ARN of the Target Group (needed by ECS)"
   value       = aws_lb_target_group.strapi_tg.arn
 }
 
-
 output "alb_dns_name" {
   description = "The DNS name of the load balancer"
-  value       = module.alb.alb_dns_name 
+  # Point to the resource 'aws_lb', not 'module.alb'
+  value       = aws_lb.strapi_alb.dns_name 
 }
--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,6 +1,5 @@
-# modules/rds/outputs.tf
-
-output "db_endpoint" {
+output "db_instance_endpoint" {
   description = "The database URL/address to pass to the Strapi containers"
+  # Use .address or .endpoint (endpoint includes the port)
   value       = aws_db_instance.strapi_db.address
 }
